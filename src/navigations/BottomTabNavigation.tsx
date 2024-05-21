c9e63b6308041053b4469f49d9b23e97a92e1b7e
Faisal <it13.knitto@gmail.com>
2024-05-21 12:00:14 +0700

    feat: home screen navigation

---
+++ src/navigations/BottomTabNavigation.tsx
@@ -0,0 +1,48 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import Home from "../screens/Home";
+import Favorite from "../screens/Favorite";
+import { Feather } from "@expo/vector-icons";
+import Search from "../screens/Search";
+import HomeStackNavigation from "./HomeStackNavigation";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigation = (): JSX.Element => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeStackNavigation}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="home" size={28} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="search" size={28} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={Favorite}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="heart" size={28} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigation;
