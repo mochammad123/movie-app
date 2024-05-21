18bd26e56d6e706157ef10871c3a08d3996cdedc
Faisal <it13.knitto@gmail.com>
2024-05-21 14:33:16 +0700

    set up env variables

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { API_ACCESS_TOKEN, API_URL } from "react-native-dotenv";
 
-const MovieDetail = ({ navigation }: any) => {
+const MovieDetail = ({ navigation }: any): any => {
+  const fetchData = (): void => {
+    if (API_URL == null || API_ACCESS_TOKEN.length == null) {
+      throw new Error("ENV not found");
+    }
+
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    };
+
+    fetch(API_URL, options)
+      .then(async (response) => await response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.marginVertical20}>Movie Detail Page</Text>
       <Button
-        title="Go to Home"
+        title="Fetch Data"
         onPress={() => {
-          navigation.navigate("Home");
+          fetchData();
         }}
       />
     </View>
