bbd3daaf0aeca4a9653c89fd77b2db29be1f831c
Faisal <it13.knitto@gmail.com>
2024-05-21 15:13:44 +0700

    feat: Home consume API

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,28 +1,55 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StatusBar, StyleSheet, View } from "react-native";
+import type { MovieListProps } from "../types/app";
+import MovieList from "../components/movies/MovieList";
 
-const Home = ({ navigation }: any) => {
+const movieLists: MovieListProps[] = [
+  {
+    title: "Now Playing in Theater",
+    path: "movie/now_playing?language=en-US&page=1",
+    coverType: "backdrop",
+  },
+  {
+    title: "Upcoming Movies",
+    path: "movie/upcoming?language=en-US&page=1",
+    coverType: "poster",
+  },
+  {
+    title: "Top Rated Movies",
+    path: "movie/top_rated?language=en-US&page=1",
+    coverType: "poster",
+  },
+  {
+    title: "Popular Movies",
+    path: "movie/popular?language=en-US&page=1",
+    coverType: "poster",
+  },
+];
+
+const Home = (): JSX.Element => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.marginVertical20}>Movie Page</Text>
-      <Button
-        title="Go to Movie Detail"
-        onPress={() => {
-          navigation.navigate("MovieDetail");
-        }}
-      />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {movieLists.map((movieList) => (
+          <MovieList
+            title={movieList.title}
+            path={movieList.path}
+            coverType={movieList.coverType}
+            key={movieList.title}
+          />
+        ))}
+        <StatusBar translucent={false} />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop: StatusBar.currentHeight ?? 32,
     alignItems: "center",
     justifyContent: "center",
-  },
-  marginVertical20: {
-    marginVertical: 20,
+    rowGap: 16,
   },
 });
 
